0e77ad9b541fe455ca6cf1ab50fc7f52e6a697e6
Constance <constance.parmentier@ynov.com>
2025-03-13 10:15:08 +0100

    ~modification de la variable insert pour changer les bon nom. + ajout fonction main

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,7 +16,7 @@ var DB *sql.DB
 // liens entre bddd et go
 func ConnectDB() {
 	// Format: "user:password@tcp(host:port)/dbname"
-	dsn := "user:password@tcp(localhost:3306)/forum?parseTime=true"
+	dsn := "root:@tcp(localhost:3306)/forum?parseTime=true"
 
 	var err error
 	DB, err = sql.Open("mysql", dsn)
@@ -24,23 +25,24 @@ func ConnectDB() {
 	}
 
 	DB.SetConnMaxLifetime(5 * time.Minute) // connecter pour 5 min
-	DB.SetMaxOpenConns(10) // 10 personnes connecter en même temps
-	DB.SetMaxIdleConns(5)// 5 connection innactive ouverte en même temps
+	DB.SetMaxOpenConns(10)                 // 10 personnes connecter en même temps
+	DB.SetMaxIdleConns(5)                  // 5 connection innactive ouverte en même temps
 
 	if err = DB.Ping(); err != nil {
 		log.Fatal("❌ Impossible de pinger la base de données:", err)
 	}
 
 	fmt.Println("Connexion réussie à MySQL !")
+
 }
 
-func Register(username, surnom, email, password string) error{
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)//hash les mdp
+func Register(username, surnom, email, password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost) //hash les mdp
 	if err != nil {
 		return err
 	}
 
-	query := "INSERT INTO utilisateur (username, surnom, email, password) VALUES (?, ?, ?, ?)"//query pour requête sql
+	query := "INSERT INTO utilisateur (nom, surnom, email, MDP) VALUES (?, ?, ?, ?)" //query pour requête sql
 	_, err = DB.Exec(query, username, surnom, email, string(hash))
 
 	return err
@@ -61,4 +63,9 @@ func Login(email, password string) error {
 		return errors.New("mot de passe incorrect")
 	}
 	return nil
+}
+
+func main() {
+	ConnectDB()
+	fmt.Println(Register("nom", "surnom", "email", "password"))
 }
